d770d33a486b9dbe40fa933fac70e7fc340dfcd7
Alexandru Branza <branza.alex@gmail.com>
2023-09-12 14:07:13 +0300

    Fix Server Config

--- server/config.js
+++ server/config.js
@@ -5,8 +5,8 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const config = {
   client_id: process.env.REACT_APP_CLIENT_ID,
-  redirect_uri: process.env.REACT_APP_CLIENT_SECRET,
-  client_secret: process.env.REACT_APP_REDIRECT_URI,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  client_secret: process.env.REACT_APP_CLIENT_SECRET,
   proxy_url: process.env.REACT_APP_PROXY_URL
 };
 
